987d518f961c7218f7dcf0ed03080e565f68305d
parisandhu <kaurparneet152@gmail.com>
2021-03-06 12:27:36 -0500

    Initial commit

---
+++ src/com/parneet/Main.java
@@ -0,0 +1,54 @@
+package com.parneet;
+import java.util.Arrays;
+import java.util.Collections;
+public class Main {
+
+    public static void main(String[] args) {
+        Integer[] intArr = new Integer[]{1, 3, 4, 2, 5, 6, 11, 18, 15, 14};
+
+        // displaying in reverse order
+        System.out.println("reverse order list");
+        for (int i = intArr.length - 1; i >= 0; i--) {
+            System.out.println(intArr[i]);
+        }
+
+        System.out.println(" Decending order list");
+
+        Arrays.sort(intArr, Collections.reverseOrder());
+
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.println(intArr[i]);
+        }
+
+        System.out.println("Ascending order list");
+
+        Arrays.sort(intArr);
+
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.println(intArr[i]);
+        }
+
+        System.out.println("ODD numbers");
+
+        for (int i = 0; i < intArr.length; i++) {
+            if (i % 2 != 0)
+                System.out.println(intArr[i]);
+        }
+
+        System.out.println("EVEN numbers");
+
+        for (int i = 0; i < intArr.length; i++) {
+            if (i % 2 == 0)
+                System.out.println(intArr[i]);
+        }
+
+        System.out.println("Sum");
+
+        int sum = 0;
+        for (int i = 0; i < intArr.length; i++) {
+            sum += intArr[i];
+        }
+        System.out.println(sum);
+    }
+}
+
